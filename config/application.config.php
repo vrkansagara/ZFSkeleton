22d4b42790a427478b45f5e721360f04582dabd4
Vallabh Kansagara <vrkansagara@gmail.com>
2015-09-30 11:09:39 +0530

    php local fixer .php_cs

--- config/application.config.php
+++ config/application.config.php
@@ -9,8 +9,7 @@ return [
     // This should be an array of module namespaces used in the application.
     'modules' => [
         'Application',
-        'Album',
-        'ZendSkeletonModule'
+        'Album'
     ],
 
     // These are various options for the listeners attached to the ModuleManager
--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,4 @@
  * file.
  */
 
-return [
-    // ...
-];
+return [];
